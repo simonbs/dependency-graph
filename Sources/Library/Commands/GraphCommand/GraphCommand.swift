10546aa077d91eadb318263c4ff5b99f5e081150
Simon Støvring <mail@simonbs.dk>
2022-12-08 15:58:36 +0100

    Switches on classified project root

--- Sources/Library/Commands/GraphCommand/GraphCommand.swift
+++ Sources/Library/Commands/GraphCommand/GraphCommand.swift
@@ -4,6 +4,17 @@ import ProjectRootClassifier
 import XcodeDependencyGraphBuilder
 import XcodeProjectParser
 
+private enum GraphCommandError: LocalizedError {
+    case unknownProject(URL)
+
+    var errorDescription: String? {
+        switch self {
+        case .unknownProject(let fileURL):
+            return "Unknown project at \(fileURL.path)"
+        }
+    }
+}
+
 public struct GraphCommand {
     private let projectRootClassifier: ProjectRootClassifier
     private let xcodeProjectParser: XcodeProjectParser
@@ -22,9 +33,17 @@ public struct GraphCommand {
 
     public func run(withInput input: String) throws {
         let fileURL = URL(filePath: input)
-        let xcodeProject = try xcodeProjectParser.parseProject(at: fileURL)
-        let graph = try xcodeDependencyGraphBuilder.buildGraph(from: xcodeProject)
-        let dotGraph = try dotGraphTransformer.transform(graph)
-        print(dotGraph)
+        let projectRoot = projectRootClassifier.classifyProject(at: fileURL)
+        switch projectRoot {
+        case .xcodeproj(let xcodeprojFileURL):
+            let xcodeProject = try xcodeProjectParser.parseProject(at: xcodeprojFileURL)
+            let graph = try xcodeDependencyGraphBuilder.buildGraph(from: xcodeProject)
+            let dotGraph = try dotGraphTransformer.transform(graph)
+            print(dotGraph)
+        case .packageSwiftFile(let packageSwiftFileURL):
+            throw GraphCommandError.unknownProject(packageSwiftFileURL)
+        case .unknown:
+            throw GraphCommandError.unknownProject(fileURL)
+        }
     }
 }
