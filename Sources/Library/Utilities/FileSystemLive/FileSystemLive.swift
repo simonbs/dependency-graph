cce994cc415af4df614d340c38a76c2e00a42fae
Simon Støvring <mail@simonbs.dk>
2022-12-08 16:04:05 +0100

    Fixes file existence check

--- Sources/Library/Utilities/FileSystemLive/FileSystemLive.swift
+++ Sources/Library/Utilities/FileSystemLive/FileSystemLive.swift
@@ -5,18 +5,18 @@ public struct FileSystemLive: FileSystem {
     public init() {}
 
     public func fileExists(at itemURL: URL) -> Bool {
-        return FileManager.default.fileExists(atPath: itemURL.absoluteString)
+        return FileManager.default.fileExists(atPath: itemURL.path)
     }
 
     public func isDirectory(at itemURL: URL) -> Bool {
         var isDirectory: ObjCBool = false
-        FileManager.default.fileExists(atPath: itemURL.absoluteString, isDirectory: &isDirectory)
+        FileManager.default.fileExists(atPath: itemURL.path, isDirectory: &isDirectory)
         return isDirectory.boolValue
     }
 
     public func contentsOfDirectory(at directoryURL: URL) -> [String] {
         do {
-            return try FileManager.default.contentsOfDirectory(atPath: directoryURL.absoluteString)
+            return try FileManager.default.contentsOfDirectory(atPath: directoryURL.path)
         } catch {
             return []
         }
