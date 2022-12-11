803af560e9b33cf6774489c57cecd18496eb048f
Simon Støvring <mail@simonbs.dk>
2022-12-11 19:25:00 +0100

    Improves error reporting

--- Sources/Library/Parsing/PackageSwiftFileParserLive/PackageSwiftParserLive.swift
+++ Sources/Library/Parsing/PackageSwiftFileParserLive/PackageSwiftParserLive.swift
@@ -3,6 +3,17 @@ import Foundation
 import PackageSwiftFile
 import PackageSwiftFileParser
 
+private enum PackageSwiftFileParserLiveError: LocalizedError {
+    case failedParsing(URL, Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .failedParsing(let fileURL, let decodingError):
+            return "Failed parsing dumped Package.swift file at \(fileURL.path): \(decodingError)"
+        }
+    }
+}
+
 public struct PackageSwiftFileParserLive: PackageSwiftFileParser {
     private let dumpPackageService: DumpPackageService
 
@@ -11,10 +22,14 @@ public struct PackageSwiftFileParserLive: PackageSwiftFileParser {
     }
 
     public func parseFile(at fileURL: URL) throws -> PackageSwiftFile {
-        let contents = try dumpPackageService.dumpPackageForSwiftPackageFile(at: fileURL)
-        let decoder = JSONDecoder()
-        let intermediate = try decoder.decode(IntermediatePackageSwiftFile.self, from: contents)
-        let mapper = PackageSwiftFileMapper(packageSwiftFileParser: self)
-        return try mapper.map(intermediate)
+        do {
+            let contents = try dumpPackageService.dumpPackageForSwiftPackageFile(at: fileURL)
+            let decoder = JSONDecoder()
+            let intermediate = try decoder.decode(IntermediatePackageSwiftFile.self, from: contents)
+            let mapper = PackageSwiftFileMapper(packageSwiftFileParser: self)
+            return try mapper.map(intermediate)
+        } catch {
+            throw PackageSwiftFileParserLiveError.failedParsing(fileURL, error)
+        }
     }
 }
