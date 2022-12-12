16739a0177359c464e5ce4a9279a8ad289746119
Simon Støvring <mail@simonbs.dk>
2022-12-12 06:39:41 +0100

    Adds formula

---
+++ Formula/dependency-graph.rb
@@ -0,0 +1,17 @@
+class DependencyGraph < Formula
+  desc "Generates graphs of the dependencies in an Xcode project or Swift package."
+  homepage "https://github.com/simonbs/DependencyGraph"
+  url "git@github.com:simonbs/DependencyGraph.git", tag: "1.0.0", using: :git
+  head "https://github.com/simonbs/DependencyGraph", branch: "main"
+
+  depends_on xcode: ["12.0", :build]
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/dependency-graph"
+  end
+
+  test do
+    system "#{bin}/dependency-graph", "--version"
+  end
+end
