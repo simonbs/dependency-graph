6c02c9472d0f922a0ca22a42297bcd81989b9088
Simon Støvring <mail@simonbs.dk>
2022-12-12 20:39:06 +0100

    Bumps version number to 1.1.0 (#7)

--- Formula/dependency-graph.rb
+++ Formula/dependency-graph.rb
@@ -1,7 +1,7 @@
 class DependencyGraph < Formula
   desc "Generates graphs of the dependencies in an Xcode project or Swift package."
   homepage "https://github.com/simonbs/DependencyGraph"
-  url "git@github.com:simonbs/dependency-graph.git", tag: "1.0.0", using: :git
+  url "git@github.com:simonbs/dependency-graph.git", tag: "1.1.0", using: :git
   head "https://github.com/simonbs/dependency-graph", branch: "main"
 
   depends_on xcode: ["12.0", :build]
